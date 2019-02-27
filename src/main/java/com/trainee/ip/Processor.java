8e7084d61d50c252065c7aa357999dc22da925bd
Prasuna <prasuna.kaveti@gmail.com>
2019-02-27 21:37:19 +0100

    Initial commit

---
+++ src/main/java/com/trainee/ip/Processor.java
@@ -0,0 +1,8 @@
+package com.trainee.ip;
+/* *
+ * Created by @ gnanaprasunakaveti on 2019-02-27
+ *
+ */
+
+public class Processor {
+}
---
+++ src/main/java/com/trainee/ip/StartApplication.java
@@ -0,0 +1,11 @@
+package com.trainee.ip;
+/* *
+ * Created by @ gnanaprasunakaveti on 2019-02-27
+ *
+ */
+
+public class StartApplication {
+    public static void main(String args[]){
+     System.out.println("Welcome to Todo List Application.");
+    }
+}
